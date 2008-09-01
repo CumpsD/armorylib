f0f0234c71f8e7bbfc6ff9b3532d0aedee454e91
cumps.david <cumps.david@telenet.be>
2008-09-01 15:06:07 +0000

    Added Initial Attribute Decoration, lot to do

---
+++ Armory/ArmoryLib/Character/RequiredDetailAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArmoryLib.Character
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    sealed class RequiredDetailAttribute : Attribute
+    {
+        private readonly CharacterDetail _requiredDetail;
+
+        public RequiredDetailAttribute(CharacterDetail requiredDetail)
+        {
+            _requiredDetail = requiredDetail;
+        }
+    }
+}
