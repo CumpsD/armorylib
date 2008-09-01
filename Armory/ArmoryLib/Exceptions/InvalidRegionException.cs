881520255560ef9fd2669beb6e00dda15e84b7eb
cumps.david <cumps.david@telenet.be>
2008-09-01 08:27:15 +0000

    Initial version

---
+++ Armory/ArmoryLib/Exceptions/InvalidRegionException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArmoryLib.Exceptions
+{
+    public class InvalidRegionException : Exception
+    {
+        public InvalidRegionException() { }
+        public InvalidRegionException(string message) : base(message) { }
+        public InvalidRegionException(string message, Exception inner) : base(message, inner) { }
+    }
+}
---
+++ Armory/ArmoryTester/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using ArmoryLib;
+
+namespace ArmoryTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Armory armory = new Armory
+            {
+                Region = Region.Europe
+            };
+
+            /*
+             * Specs:
+             *  - Character
+             *      - SearchCharacter(string name)
+             *      - LoadCharacter(string name, string realm)
+             *  - Guild
+             *      - SearchGuild(string name)
+             *      - LoadGuild(string name, string realm)
+             *  - Item?
+             *      - LoadItem(????)
+             * */
+        }
+    }
+}
