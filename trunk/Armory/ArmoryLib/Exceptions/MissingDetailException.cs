6232bfa20c23604d4e8d5ecea15596bc1e254d95
cumps.david@telenet.be <cumps.david@telenet.be@38220f3e-d155-0410-bc70-4bef5cb7ba51>
2008-09-03 07:42:07 +0000

    Added Detail Checks. Removed the Attributes, since it proved to be too much overhead for what it's worth. (No modifying of existing assemblies with LCG, Reflection.Emit, or too much overhead from Reflection, AOP)

---
+++ trunk/Armory/ArmoryLib/Exceptions/MissingDetailException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArmoryLib.Exceptions
+{
+    public class MissingDetailException : Exception
+    {
+        public MissingDetailException() { }
+        public MissingDetailException(string message) : base(message) { }
+        public MissingDetailException(string message, Exception inner) : base(message, inner) { }
+    }
+}
